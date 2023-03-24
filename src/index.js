f6b3d218ccb9581311d0dab0fcd62c18c87ee65a
smmartin1 <smarie333m@gmail.com>
2023-03-24 09:58:43 -0400

    Added Container and Router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Container from 'react-bootstrap/Container';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 //import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <Container>
+        <App />
+      </Container>
+    </React.StrictMode>
+  </BrowserRouter> 
 );
 
 // If you want to start measuring performance in your app, pass a function
